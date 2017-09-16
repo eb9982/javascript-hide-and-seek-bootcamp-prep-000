c0b8f16cd05c76984a41a3c2d31d57f97aa4c29c
Luke Borsare <lborsare1@gmail.com>
2017-09-16 17:16:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  const element = document.getElementById('nested')
+  return element.children[0]
+}
+function deepestChild() {
+  var element = document.getElementById('grand-node')
+  var next = element.children[0]
+  while (next) {
+    element = next
+    next = element.children[0]
+  }
+  return element
+}
+function increaseRankBy(n) {
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (var i = 0; i < list.length; i++) {
+     list[i].innerHTML = parseInt(list[i].innerHTML) + n
+  }
+}
